fc1116ac66272a3573613958fd54c0706d921cdf
Alberto Furlan <alberto2.furlan@mail.polimi.it>
2024-12-04 15:03:59 +0100

    added draft implementations in cpp and cuda

---
+++ src/cpp-impl/cpp_implementation.h
@@ -0,0 +1,8 @@
+//
+// Created by Alberto Furlan on 04/12/24.
+//
+
+#ifndef CPP_IMPLEMENTATION_H
+#define CPP_IMPLEMENTATION_H
+
+#endif //CPP_IMPLEMENTATION_H
---
+++ src/cuda-impl/cuda_implementation.cu
@@ -0,0 +1,5 @@
+//
+// Created by Alberto Furlan on 04/12/24.
+//
+
+#include "cuda_implementation.cuh"
