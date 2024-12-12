9ceb992bef0a23ee13de5fccff9ec4532b91ade3
Alberto Furlan <alberto2.furlan@mail.polimi.it>
2024-12-12 17:45:09 +0100

    checked out fix from v1 (w. related CMakeLists)

--- src/cuda-impl/cuda_implementation.cuh
+++ src/cuda-impl/cuda_implementation.cuh
@@ -8,9 +8,15 @@
 #include <iostream>
 #include <vector>
 #include "cuda_runtime.h"
+#include <array>
+#include <cassert>
+#include <iostream>
+#include <cstdlib>
 
 int check_devices_status();
 
 int run_cuda_main();
+
+int run_assignment_cuda(int);
 
 #endif //CUDA_IMPLEMENTATION_CUH
