68b85c244c443dff9d2d2e4a2641ad6672c6b070
Alberto Furlan <alberto2.furlan@mail.polimi.it>
2025-01-29 12:29:42 +0100

    tiny main refactor

--- main.cpp
+++ main.cpp
@@ -13,47 +13,50 @@ int main(int argc, char** argv)
     std::cin >> value;
 
     std::cout << "[Generator]:: initializing input and filter\n";
-    auto [input, filter] = generate(value);
-    std::cout << "\n";
-    std::cout << "\n";
+    try
+    {
+        auto [input, filter] = generate(value);
 
-    if (input == nullptr || filter == nullptr)
+        std::cout << "\n";
+        std::cout << "\n";
+
+        std::cout << "[Test]:: cpu convolution\n";
+        auto output = assignment_main(value, input, filter);
+        log_array(output);
+        delete[] output;
+        std::cout << "\n";
+        std::cout << "\n";
+        std::cout << "[Test]:: basic convolution\n";
+        output = main_basic(value, input, filter);
+        log_array(output);
+        delete[] output;
+        std::cout << "\n";
+        std::cout << "\n";
+        std::cout << "[Test]:: tiling convolution\n";
+        output = main_tiling(value, input, filter);
+        log_array(output);
+        delete[] output;
+        std::cout << "\n";
+        std::cout << "\n";
+        std::cout << "[Test]:: streams convolution\n";
+        output = main_streams(value, input, filter);
+        log_array(output);
+        delete[] output;
+        std::cout << "\n";
+        std::cout << "\n";
+        std::cout << "[Test]:: streams + tiling convolution\n";
+        output = main_tiling_streams(value, input, filter);
+        log_array(output);
+        delete[] output;
+
+        delete[] input;
+        delete[] filter;
+
+        return 0;
+    }
+    catch (const std::exception& e)
     {
         std::cerr << "Error: input/filter initialization failed\n";
         return 1;
     }
-
-    std::cout << "[Test]:: cpu convolution\n";
-    auto output = assignment_main(value, input, filter);
-    log_array(output);
-    delete[] output;
-    std::cout << "\n";
-    std::cout << "\n";
-    std::cout << "[Test]:: basic convolution\n";
-    output = main_basic(value, input, filter);
-    log_array(output);
-    delete[] output;
-    std::cout << "\n";
-    std::cout << "\n";
-    std::cout << "[Test]:: tiling convolution\n";
-    output = main_tiling(value, input, filter);
-    log_array(output);
-    delete[] output;
-    std::cout << "\n";
-    std::cout << "\n";
-    std::cout << "[Test]:: streams convolution\n";
-    output = main_streams(value, input, filter);
-    log_array(output);
-    delete[] output;
-    std::cout << "\n";
-    std::cout << "\n";
-    std::cout << "[Test]:: streams + tiling convolution\n";
-    output = main_tiling_streams(value, input, filter);
-    log_array(output);
-    delete[] output;
-
-    delete[] input;
-    delete[] filter;
-
-    return 0;
 }
